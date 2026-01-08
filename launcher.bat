3cacd9b9b6cf1aeeffefb49caf293840a0bf0047
simon-ecke <simon.ecke@gmx.de>
2026-01-08 14:32:02 +0100

    Update requirements.txt and fix launcher

--- launcher.bat
+++ launcher.bat
@@ -5,7 +5,12 @@ REM Go to repo folder (where this .bat is)
 cd /d "%~dp0"
 
 REM --- Activate conda env (EDIT env name if needed) ---
-call conda activate peakfinder
+REM Option 1: If conda is in your PATH, use this:
+call conda.bat activate peakfinder
+
+REM Option 2: Or uncomment and edit this line with your conda path:
+REM call "%USERPROFILE%\Anaconda3\Scripts\activate.bat" peakfinder
+REM call "%USERPROFILE%\Miniconda3\Scripts\activate.bat" peakfinder
 
 REM --- Run Streamlit ---
 python -m streamlit run app.py --server.address 127.0.0.1 --browser.gatherUsageStats false
